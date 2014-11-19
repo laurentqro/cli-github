9b0fdf214933966d278704a0381fa6516cadeb95
Laurent Curau <laurentcurau@gmail.com>
2014-11-19 17:40:53 +0000

    Fix test

--- features/step_definitions/github_client_steps.rb
+++ features/step_definitions/github_client_steps.rb
@@ -2,7 +2,7 @@ require_relative "../../lib/user"
 
 When(/^I request the favourite language for "(.*?)"$/) do |username|
 	user = GithubCli::User.new(username: 'laurentqro', github_client: @github_client)
-	@app.print_favourite_language_for(user)
+	@app.print_favourite_language_for(username)
 end
 
 Then(/^I should see "(.*?)"$/) do |string|
