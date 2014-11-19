b46ac6ec558d5f05ba76f94bda2ec3d468c7e69e
Laurent Curau <laurentcurau@gmail.com>
2014-11-19 17:39:49 +0000

    Format output

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -5,8 +5,9 @@ module GithubCli
 	    @github_client = github_client
 	  end
 
-	  def print_favourite_language_for(user)
-	  	@stdout.puts "#{user.username}'s favourite programming language is #{user.favourite_language}."
+	  def print_favourite_language_for(username)
+	  	user = GithubCli::User.new(username: username, github_client: @github_client)
+	  	@stdout.puts "#{username}'s favourite programming language is #{user.favourite_language}."
 	  end
 	end
 end
