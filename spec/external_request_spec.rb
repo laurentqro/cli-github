c5deb173ca998c94494ca484254b8b219d3fbc68
Laurent Curau <laurentcurau@gmail.com>
2014-11-19 12:38:31 +0000

    Create fake Github service

--- spec/external_request_spec.rb
+++ spec/external_request_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe "External request" do
   it "queries laurentqro's repos on GitHub" do
-    uri = URI("https://api.github.com/users/laurentqro/repos?access_token=#{ENV["GITHUB_API_KEY"]}")
+    uri = URI("https://api.github.com/users/laurentqro/repos")
+    response = JSON.load(Net::HTTP.get(uri))
 
-    response = Net::HTTP.get(uri)
-
-    expect(response).to be_an_instance_of(String)
+    expect(response[0]["full_name"]).to eql "laurentqro/awesome-ruby"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
+require 'json'
 require 'webmock/rspec'
+require_relative 'support/fake_github.rb'
 WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
+
+	config.before(:each) do
+		stub_request(:any, /api.github.com/).to_rack(FakeGitHub)
+	end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -9,12 +16,4 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
-	config.before(:each) do
-		json_data = File.open('spec/support/repos.json').read
-
-		stub_request(:get, /api.github.com/).
-			with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-			to_return(status: 200, body: json_data, headers: {})
-	end
 end
