53898e9803f4630db8897d294bc210b51dcf1e72
Laurent Curau <laurentcurau@gmail.com>
2014-11-25 17:13:36 +0000

    Refactor user specs

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,15 +4,11 @@ require_relative '../lib/user'
 describe GithubCli::User do
 	subject(:user) { GithubCli::User.new(username: 'laurentqro', github_client: GithubCli::GithubClient.new) }
 
-	it "has a username" do
-		expect(subject).to respond_to :username
-	end
+	it { is_expected.to respond_to :username }
 
-	it "has a favourite language" do
-		expect(subject).to respond_to :favourite_language
-	end
+	it { is_expected.to respond_to :favourite_language }
 
-	it "has Ruby for favourite language" do
+	it "should have Ruby for favourite language" do
 		expect(subject.favourite_language).to eql "Ruby"
 	end
 
