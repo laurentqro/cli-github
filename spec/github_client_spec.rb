5d816e4a81c5a037e836773d833d1b0fcc4bcc36
Laurent Curau <laurentcurau@gmail.com>
2014-11-25 17:09:44 +0000

    Use Webmock to mock the github API and return a JSON fixture

--- spec/github_client_spec.rb
+++ spec/github_client_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require_relative '../lib/application'
 
 describe "The Github client" do
-	it "should get the requested user's repos" do
+	it "requests laurentqro's repos" do
 		client = GithubCli::GithubClient.new
 		user = GithubCli::User.new(username: "laurentqro", github_client: client)
 		repos = client.get_repos_for(user)
@@ -10,5 +10,8 @@ describe "The Github client" do
 		expect(repos).to be_a Array
 		expect(repos.length).to eql 16
 		expect(repos.first["name"]).to eql "awesome-ruby"
+
+		stub = stub_request(:get, "https://api.github.com/users/laurentqro/repos")
+    expect(stub).to have_been_requested
 	end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,19 @@
 require 'json'
 require 'webmock/rspec'
-require_relative 'support/fake_github.rb'
 WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
 
-	config.before(:each) do
-		stub_request(:any, /api.github.com/).to_rack(FakeGitHub)
-	end
+  config.before(:each) do
+    repos_json_file = File.open('spec/support/fixtures/repos.json').read
 
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
+    stub_request(:get, "https://api.github.com/users/laurentqro/repos").
+    with(:headers => { 'User-Agent' => 'Test' }).
+    to_return(:status => 200, :body => repos_json_file, headers: { 'Content-Type' => 'application/json' })
   end
 end
