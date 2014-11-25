8367a7d5c941f604c969549d589d81f9dc875899
Laurent Curau <laurentcurau@gmail.com>
2014-11-25 17:26:09 +0000

    Refactor the tests for GithubClient

--- spec/github_client_spec.rb
+++ spec/github_client_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 require_relative '../lib/application'
 
-describe "The Github client" do
-	it "requests laurentqro's repos" do
-		client = GithubCli::GithubClient.new
-		user = GithubCli::User.new(username: "laurentqro", github_client: client)
-		repos = client.get_repos_for(user)
+describe GithubCli::GithubClient do
+	subject(:client) { GithubCli::GithubClient.new }
 
-		expect(repos).to be_a Array
-		expect(repos.length).to eql 16
-		expect(repos.first["name"]).to eql "awesome-ruby"
+	context "when laurentqro's repos are requested" do
+    let(:user) { GithubCli::User.new(username: "laurentqro", github_client: client) }
+    let(:repos) { client.get_repos_for(user) }
 
-		stub = stub_request(:get, "https://api.github.com/users/laurentqro/repos")
-    expect(stub).to have_been_requested
-	end
+		it "makes a request to Github" do
+      client.get_repos_for(user)
+			stub = stub_request(:get, "https://api.github.com/users/laurentqro/repos")
+	    expect(stub).to have_been_requested
+	  end
+
+	  it "returns an array" do
+      expect(repos).to be_a Array
+	  end
+
+    it "contains 16 items" do
+      expect(repos.length).to eql 16
+    end
+
+    it "has a first repo called awesome-ruby" do
+      expect(repos.first["name"]).to eql "awesome-ruby"
+    end
+  end
 end
